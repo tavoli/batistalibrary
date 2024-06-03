e6bfeed5d94f3710b966b7e64298063d13bdd246
Gustavo Lima <gustavolima@lima.local>
2024-06-03 16:01:48 -0300

    feat: home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="">
+  <img class="w-full object-cover" src="/header.png" />
+  <h1 class="text-[#8B1300] text-4xl font-light self-center text-center py-16">Biblioteca</h1>
+  <p class="text-center px-12">Desbrave sua biblioteca com facilidade - organize e controle empréstimos</p>
+  <div class="self-end flex flex-col py-16 gap-2 mx-16">
+    <button class="btn btn-primary">entrar</button>
+    <button class="btn btn-primary">cadastrar</button>
+  </div>
+</main>
+
+<style>
+  main {
+     display: grid;
+     grid-template-rows: auto auto auto 1fr;
+     height: 100vh;
+  }
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        red: {
+          300: '#E79D80',
+          400: '#E69879',
+          500: '#E04500',
+          600: '#CF3B00',
+          700: '#8B1300',
+        },  
+      },
+    }
+  },
+  plugins: []
+};
